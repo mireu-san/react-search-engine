23e653bc7872f8d61d35c7193ce8d5139bd46c8a
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-27 16:29:48 +0900

    header.js, and sidebar.js(initial)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react';
+import Header from "./components/Header";
+
 function App() {
-  return (
-    <div className="App">
-      test (app.js)
-    </div>
-  );
+	const [animeList, setAnimeList] = useState([]); // empty array
+	const [topAnime, setTopAnime] = useState([]); // empty array
+	const [search, setSearch] = useState(''); // empty string
+
+	return (
+		<div className="App">
+			<Header />
+			<div className="content-wrap">
+				<Sidebar 
+					topAnime={topAnime} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>Anime Finder</h1>
+            <h3>Simply input the first word of your favourite novel or manga.</h3> 
+        </header>
+    )
+}
+
+export default Header
